c1f50bc04f9f7b1ae09f707ca2f2003629547487
ncnguyen1314 <75525938+ncnguyen1314@users.noreply.github.com>
2021-11-16 15:14:42 +0700

    Chỉnh sửa một số từ

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
+//Mongoose(to install: npm install mongoose):
+const mongoose = require('mongoose');
+const _schema = require('./models/schema')
 //Connect to mongodb:
-const dbURI = 'mongodb+srv://admin:ptudw19@cluster0.hbgza.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://admin:ptudw19@cluster0.hbgza.mongodb.net/Ogini-ShoppingCartWebsite?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+    .then((result) =>console.log("Connected to database"))
+    .catch((err)=>console.log(err));
+main.get('/add-item',(req,res)=>{
+    const item = new _scheme({
+        name:'Vegetable',
+        Quantity: 69,
+        Price: 60000
+    });
+
+    item.save()
+        .then((result) =>{
+            res.send(result)
+        })
+        .catch((err)=>{
+            console.log(err)
+        } );
+})
 
 (function ($) {
     "use strict";
 
-    
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
