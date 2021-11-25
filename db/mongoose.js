81f46fd81ae15e629ed7c3cf181600e39b37c043
Doan <doanak000@gmail.com>
2021-11-25 15:12:36 +0700

    refix db

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
-
-try {
-  mongoose.connect(
-    "mongodb+srv://admin:ptudw19@cluster0.hbgza.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    },
-    () => console.log("connect sucess")
-  );
-} catch (e) {
-  console.log("connect fail");
-}
+mongoose.connect(
+  "mongodb+srv://admin:ptudw19@cluster0.hbgza.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
