55c37cd920fe2e690cfc56ccd981a227e1c3533f
Doan <doanak000@gmail.com>
2021-11-25 18:00:53 +0700

    complete all api

--- routes/item.js
+++ routes/item.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Item = require("../models/item");
 const router = new express.Router();
 const PAGE_SIZE = 5;
+//push item lên
 router.post("/items", async (req, res) => {
   const item = new Item(req.body);
   try {
@@ -11,94 +12,86 @@ router.post("/items", async (req, res) => {
     res.status(400).send(e);
   }
 });
-
+//lấy item về
 router.get("/items", (req, res) => {
   // nếu chỉ /items thì sẽ show all data
   // nếu /items?page=1 thì sẽ show 5 data đầu tiên, ?page=2 thì show 5 data tiếp theo và cứ vậy, mún đổi số data render ra thì đổi cái page_size
+  // nếu items?name=abc thì sẽ filter ra data có name là abc
+  //nếu items?name=abc&&page=1 thì sẽ filter có phân trang
   var page = req.query.page;
-  if (page) {
-    page = parseInt(page);
-    var skip = (page - 1) * PAGE_SIZE;
-    Item.find({})
-      .skip(skip)
-      .limit(PAGE_SIZE)
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.status(500).json("loi server");
-      });
+  var item = req.query.item;
+  if (item) {
+    if (page) {
+      page = parseInt(page);
+      var skip = (page - 1) * PAGE_SIZE;
+      Item.find({ item: "ves" })
+        .skip(skip)
+        .limit(PAGE_SIZE)
+        .then((items) => {
+          res.send(items);
+        })
+        .catch((err) => {
+          res.status(500).json("loi server");
+        });
+    } else {
+      Item.find({ item: "ves" })
+        .then((items) => {
+          res.send(items);
+        })
+        .catch((err) => {
+          res.status(500).json("loi server");
+        });
+    }
   } else {
-    Item.find({})
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.status(500).json("loi server");
-      });
+    if (page) {
+      page = parseInt(page);
+      var skip = (page - 1) * PAGE_SIZE;
+      Item.find({})
+        .skip(skip)
+        .limit(PAGE_SIZE)
+        .then((items) => {
+          res.send(items);
+        })
+        .catch((err) => {
+          res.status(500).json("loi server");
+        });
+    } else {
+      Item.find({})
+        .then((items) => {
+          res.send(items);
+        })
+        .catch((err) => {
+          res.status(500).json("loi server");
+        });
+    }
   }
 });
+//update
+router.patch("/items/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["quantity", "price", "item", "imageLink"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-// router.get("/tasks/:id", auth, async (req, res) => {
-//   const _id = req.params.id;
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
 
-//   try {
-//     const task = await Task.findOne({ _id, owner: req.user._id });
+  try {
+    const item = await Item.findOne({
+      _id: req.params.id,
+    });
 
-//     if (!task) {
-//       return res.status(404).send();
-//     }
+    if (!item) {
+      return res.status(404).send();
+    }
 
-//     res.send(task);
-//   } catch (e) {
-//     res.status(500).send();
-//   }
-// });
-
-// router.patch("/tasks/:id", auth, async (req, res) => {
-//   const updates = Object.keys(req.body);
-//   const allowedUpdates = ["description", "completed"];
-//   const isValidOperation = updates.every((update) =>
-//     allowedUpdates.includes(update)
-//   );
-
-//   if (!isValidOperation) {
-//     return res.status(400).send({ error: "Invalid updates!" });
-//   }
-
-//   try {
-//     const task = await Task.findOne({
-//       _id: req.params.id,
-//       owner: req.user._id,
-//     });
-
-//     if (!task) {
-//       return res.status(404).send();
-//     }
-
-//     updates.forEach((update) => (task[update] = req.body[update]));
-//     await task.save();
-//     res.send(task);
-//   } catch (e) {
-//     res.status(400).send(e);
-//   }
-// });
-
-// router.delete("/tasks/:id", auth, async (req, res) => {
-//   try {
-//     const task = await Task.findOneAndDelete({
-//       _id: req.params.id,
-//       owner: req.user._id,
-//     });
-
-//     if (!task) {
-//       res.status(404).send();
-//     }
-
-//     res.send(task);
-//   } catch (e) {
-//     res.status(500).send();
-//   }
-// });
-
+    updates.forEach((update) => (item[update] = req.body[update]));
+    await item.save();
+    res.send(item);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 module.exports = router;
